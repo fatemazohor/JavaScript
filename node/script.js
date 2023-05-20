b7b24391ff2072043276c9b224cb87b95df411a1
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-05-20 12:53:56 +0600

    JSoperators_20.05.23

---
+++ node/script.js
@@ -0,0 +1,34 @@
+let x=10;
+let y=20;
+let result=x+y;
+console.log(result);
+let m=y-x;
+console.log(m);
+
+let radius=5;
+let pi=3.1415;
+let area=pi*Math.pow(radius,2);
+console.log(area);
+let areaOfCricle=Math.PI*Math.pow(radius,2);
+console.log(areaOfCricle);
+// pi=3.1416 is Math.PI
+// pow means power, Math.pow(radius value,power value)
+
+
+// Comparison Operators(< > == != >= <=), result will be boolean (true,false).
+
+let e=23;
+let f=34;
+
+console.log(e<f);
+console.log(e>f);
+console.log(e<=f);
+console.log(e>=f);
+console.log(e!=f);
+console.log(e==f);
+
+// Logicla Operators
+// and(&&), or(||), not(!)
+// username="java" && password="898" && (gender="male" || gender="female") && country="Bangladesh"
+
+

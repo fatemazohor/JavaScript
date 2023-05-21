a737ff2f184538556e99491dfaeda03efcd5026f
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-05-21 13:05:39 +0600

    if, loop condition21.05.23

---
+++ node/if_statement.js
@@ -0,0 +1,59 @@
+let x=10;
+let y=20;
+
+if(x==y){
+console.log("True");
+
+}
+else{
+console.log("False");
+}
+// Find odd or even number.
+let d=19;
+
+if(d%2==0){
+console.log("Even");
+}
+else{
+console.log("Odd");
+}
+
+// Find number that is when divided by 3 left 1 reminder.
+let s=19;
+
+if(s%3==1){
+console.log("True");
+}
+else{
+console.log("False");
+}
+// Weather check .
+// 0> invalid ,0-18 cool, 19-27 normal, 28<= abnormal.
+let weather=27;
+
+if(weather<0){
+console.log("invalid weather");
+}
+else if(weather<=18){
+console.log("cool weather");    
+}
+else if(weather<=27){
+console.log("normal weather");    
+}
+else{
+console.log("abnorml weather");
+}
+// weather 02
+if(weather>27){
+    console.log("abnormal weather");
+
+}
+else if(weather>18){
+    console.log("normal weather");
+}
+else if(weather>=0){
+    console.log("cool weather");
+}
+else{
+console.log("invalid weather");
+}
---
+++ node/loop_condition.js
@@ -0,0 +1,41 @@
+// Loop practise.
+for(let i=100; i>=90; i--){
+    console.log(i);
+}
+// Find odd / even number in 100.
+for(let i=1; i<=100; i++){
+    if(i%2==0){
+        
+    }
+    else{
+        console.log(i);
+    }
+}
+// 1-50, divided by 3 or 5.
+for(let i=1;  i<=50; i++){
+    if(i%3==0 || i%5==0) {
+        console.log(i);
+    }
+   
+    else{
+
+    }
+}
+// 1-50, divided by 3 and 5.
+for(let i=1;  i<=50; i++){
+    if(i%3==0 && i%5==0) {
+        console.log(i);
+    }
+   
+    else{
+
+    }
+}
+// 
+let input=5;
+let result=1;
+for(let i=input; i>0; i--){
+    // result=result*i;
+     result*=i;
+}
+console.log(result);
